140ac3b7661e4d998954062a61c69139cfb73e5e
Eduardaskuliesa <eduardas2000@mail.ru>
2025-09-06 17:24:49 +0300

    feeat: Added simple UI for a timer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default async function Home() {
 
   console.log(timers);
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center p-24 bg-gray-100 dark:bg-gray-900">
+    <div className="flex font-family-times  min-h-screen flex-col items-center justify-center p-24 bg-gray-100 dark:bg-gray-900">
       <h1 className="text-4xl font-bold mb-8 text-gray-900 dark:text-gray-100">
         Welcome to Next.js with Cloudflare Workers!
       </h1>
--- src/app/timer/page.tsx
+++ src/app/timer/page.tsx
@@ -1,9 +1,62 @@
-import React from 'react'
+"use client";
+import React, { useState } from "react";
 
-const page = () => {
+function SingleDigit({ digit }) {
   return (
-    <div>page</div>
-  )
+    <div className="text-gray-100 bg-gray-900 p-4 rounded-lg text-8xl font-bold font-family-times  flex items-center justify-center">
+      {digit}
+    </div>
+  );
 }
 
-export default page
+function TimerDigitGroup({ value, label }) {
+  const tens = Math.floor(value / 10);
+  const ones = value % 10;
+
+  return (
+    <div className="flex flex-col items-center">
+      <div className="flex gap-2">
+        <SingleDigit digit={tens} />
+        <SingleDigit digit={ones} />
+      </div>
+      <p className="text-lg mt-2">{label}</p>
+    </div>
+  );
+}
+
+const TimerPage = ({
+  initialSeconds = 1,
+  initialMinutes = 25,
+  initialHours = 1,
+} = {}) => {
+  const [seconds, setSeconds] = useState(initialSeconds);
+  const [minutes, setMinutes] = useState(initialMinutes);
+  const [hours, setHours] = useState(initialHours);
+
+  const hideHours = hours === 0;
+  const hideSeconds = hours !== 0;
+
+  return (
+    <div className="flex h-[60vh] justify-center items-center">
+      <div className="flex flex-row gap-2 items-center">
+        {!hideHours && (
+          <>
+            <TimerDigitGroup value={hours} label="Hours" />
+            <div className="text-6xl font-bold mb-10">:</div>
+          </>
+        )}
+
+        <TimerDigitGroup value={minutes} label="Minutes" />
+
+        {!hideSeconds && (
+          <>
+            <div className="text-6xl font-bold mb-15">:</div>
+            <TimerDigitGroup value={seconds} label="Seconds" />
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default TimerPage;
